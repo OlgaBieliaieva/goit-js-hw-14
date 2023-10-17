e441bdbd9042d77e2e088308c74a9fda968470de
OlgaBieliaieva <belyaevaolya0@gmail.com>
2023-10-17 11:36:26 +0300

    finished 5 from 10 tasks

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsListRef = document.querySelector("#ingredients");
+
+const elements = ingredients.map((ingredient) => {
+  const listItemEl = document.createElement("li");
+  listItemEl.textContent = ingredient;
+  listItemEl.classList.add("item");
+  return listItemEl;
+});
+
+ingredientsListRef.append(...elements);
